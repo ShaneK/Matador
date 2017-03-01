31ad7ff708fdaa9e9ce03c0712cc0b6379b428d6
Ryan Dick <ryan@mortimerharvey.com>
2017-03-01 09:57:45 +0200

    increased refresh rate to avoid overwhelming backend

--- public/js/DataModel.js
+++ public/js/DataModel.js
@@ -1,48 +1,48 @@
-var DataModel = function(){
-    var _self = this;
+var DataModel = function () {
+	var _self = this;
 
-    //Page information
-    _self.complete = ko.observable(null);
-    _self.failed = ko.observable(null);
-    _self.active = ko.observable(null);
-    _self.pending = ko.observable(null);
-    _self.delayed = ko.observable(null);
-    _self.stuck = ko.observable(null);
-    _self.queues = ko.observable(null);
-    _self.keys = ko.observableArray([]);
-    _self.memory = ko.observable({});
-    _self.peakMemory = ko.observable("");
+	//Page information
+	_self.complete = ko.observable( null );
+	_self.failed = ko.observable( null );
+	_self.active = ko.observable( null );
+	_self.pending = ko.observable( null );
+	_self.delayed = ko.observable( null );
+	_self.stuck = ko.observable( null );
+	_self.queues = ko.observable( null );
+	_self.keys = ko.observableArray( [] );
+	_self.memory = ko.observable( {} );
+	_self.peakMemory = ko.observable( "" );
 
-    _self.autoRefreshId = null;
-    _self.fn = {
-        refreshViewModel: function(force){
-            var pathname = window.location.pathname.replace(window.basepath, '');
-            var refreshUrl = window.basepath + '/api' + (pathname !== '/' ? pathname : '');
+	_self.autoRefreshId = null;
+	_self.fn = {
+		refreshViewModel: function ( force ) {
+			var pathname = window.location.pathname.replace( window.basepath, '' );
+			var refreshUrl = window.basepath + '/api' + ( pathname !== '/' ? pathname : '' );
 
-            var refresh = function(){
-                $.getJSON(refreshUrl).done(function(data){
-                    _self.complete(" ("+data.counts.complete+")");
-                    _self.failed(" ("+data.counts.failed+")");
-                    _self.active(" ("+data.counts.active+")");
-                    _self.pending(" ("+data.counts.pending+")");
-                    _self.delayed(" ("+data.counts.delayed+")");
-                    _self.stuck(" ("+data.counts.stuck+")");
-                    _self.keys(data.keys);
-                    if(data.memory){
-                        _self.memory(data.memory.usage);
-                        _self.peakMemory(data.memory.peak.human);
-                    }
-                });
-            };
-            if(force){
-                clearInterval(_self.autoRefreshId);
-                refresh();
-            }
-            _self.autoRefreshId = setInterval(refresh, 2500);
-        }
-    }
+			var refresh = function () {
+				$.getJSON( refreshUrl ).done( function ( data ) {
+					_self.complete( " (" + data.counts.complete + ")" );
+					_self.failed( " (" + data.counts.failed + ")" );
+					_self.active( " (" + data.counts.active + ")" );
+					_self.pending( " (" + data.counts.pending + ")" );
+					_self.delayed( " (" + data.counts.delayed + ")" );
+					_self.stuck( " (" + data.counts.stuck + ")" );
+					_self.keys( data.keys );
+					if ( data.memory ) {
+						_self.memory( data.memory.usage );
+						_self.peakMemory( data.memory.peak.human );
+					}
+				} );
+			};
+			if ( force ) {
+				clearInterval( _self.autoRefreshId );
+				refresh();
+			}
+			_self.autoRefreshId = setInterval( refresh, 5000 );
+		}
+	}
 
-    return _self;
+	return _self;
 }
 
 var dataModel = new DataModel();
